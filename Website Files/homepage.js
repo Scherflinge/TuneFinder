b6e4b35593fc672aa00b4db0de09f08e5fd7307b
Eric Scherfling <scherflinge@wit.edu>
2020-07-08 17:03:56 -0400

    Adding Website Files

---
+++ Website Files/homepage.js
@@ -0,0 +1,18 @@
+alert("hello")
+var k;
+k=document.getElementById("userfile");
+k.onchange=function(){
+	alert(k.value)
+	//	href="\Users\thain2\Documents\Senior project\audiosearch.html"
+};
+
+/*
+ask jons:
+*get and post
+*file query javascript
+*ask if we need to use php
+Look up:
+*upload file to back end
+heroku;
+pass: tunefinder20!
+*>
